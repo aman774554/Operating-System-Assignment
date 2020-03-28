0ba6a363d2c72ce964c9cad51e2e0c715ab8aaca
aman774554 <62074888+aman774554@users.noreply.github.com>
2020-03-28 10:57:19 +0530

    Update and rename Aman_os to aman_os.c

--- aman_os.c
+++ aman_os.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 struct process
 {
-    int pro_name;
-    int arrT, waiturnT, turnT, priority,burT, burTcopy;
+    int process_name;
+    int arrival_time, waiting_time, turn_time, priority,burst_time, burst_timecopy;
 }queue1[10],queue2[10];
 int main()
 {
@@ -16,15 +16,15 @@ int main()
     {
     	printf("\nEnter Process name:-");
     	fflush(stdin);
-        scanf("%d",&queue1[i].pro_name);
-        printf("\nEnter Details For processor %d:\n",queue1[i].pro_name);
+        scanf("%d",&queue1[i].process_name);
+        printf("\nEnter Details For processor %d:\n",queue1[i].process_name);
         printf("Enter Arrival Time:-");
         fflush(stdin);
-        scanf("%d",&queue1[i].arrT);
+        scanf("%d",&queue1[i].arrival_time);
         printf("Enter Burst Time:-");
         fflush(stdin);
-        scanf("%d",&queue1[i].burT);
-        queue1[i].burTcopy=queue1[i].burT;
+        scanf("%d",&queue1[i].burst_time);
+        queue1[i].burst_timecopy=queue1[i].burst_time;
         printf("Enter Priority:\t");
         fflush(stdin);
         scanf("%d",&queue1[i].priority);
@@ -39,24 +39,24 @@ int main()
         pos=i;
         for(j=i+1;j<totalProcess;j++)
         {
-            if(queue1[j].arrT<queue1[pos].arrT)
+            if(queue1[j].arrival_time<queue1[pos].arrival_time)
                 pos=j;
         }
         temp=queue1[i];
         queue1[i]=queue1[pos];
         queue1[pos]=temp;
     }
-    time=queue1[0].arrT;
+    time=queue1[0].arrival_time;
     for(i=0;totalProcess!=0;i++)
     {
     	while(count!=t1)
     	{
     		count++;
-    		if(queue1[i].arrT<=time)
+    		if(queue1[i].arrival_time<=time)
     		{
     			for(j=i+1;j<totalProcess;j++)
     			{
-    				if(queue1[j].arrT==time && queue1[j].priority<queue1[i].priority)//pr<
+    				if(queue1[j].arrival_time==time && queue1[j].priority<queue1[i].priority)//pr<
     				{
     					queue2[pf2]=queue1[i];
 						pf2++;
@@ -70,14 +70,14 @@ int main()
 				}
 			}
 			time++;
-			queue1[i].burT--;
-			if(queue1[i].burT==0)
+			queue1[i].burst_time--;
+			if(queue1[i].burst_time==0)
 			{
-				queue1[i].turnT=time-queue1[i].arrT;
-				queue1[i].waiturnT=queue1[i].turnT-queue1[i].burTcopy;
-				printf("%d\t|\t%d\t|\t%d\n",queue1[i].pro_name,queue1[i].turnT,queue1[i].waiturnT);
-				wait_time+=time-queue1[i].waiturnT; 
-    			turnaround_time+=time-queue1[i].turnT;
+				queue1[i].turn_time=time-queue1[i].arrival_time;
+				queue1[i].waiting_time=queue1[i].turn_time-queue1[i].burst_timecopy;
+				printf("%d\t|\t%d\t|\t%d\n",queue1[i].process_name,queue1[i].turn_time,queue1[i].waiting_time);
+				wait_time+=time-queue1[i].waiting_time; 
+    			turnaround_time+=time-queue1[i].turn_time;
     			for(k=i;k<totalProcess-1;k++)
     				queue1[k]=queue1[k+1];i--;
     			totalProcess--;
@@ -85,7 +85,7 @@ int main()
 			}
 		}
 		count=0;
-		if(queue1[i].burT!=0)
+		if(queue1[i].burst_time!=0)
 		{
 			queue2[pf2]=queue1[i];
 			pf2++;
@@ -100,25 +100,25 @@ int main()
 	totalProcess2=pf2;
 	for(count=0;totalProcess2!=0;) 
 	{ 
-		if(queue2[count].burT<=t2&&queue2[count].burT>0) 
+		if(queue2[count].burst_time<=t2&&queue2[count].burst_time>0) 
     	{ 
-    		time+=queue2[count].burT; 
-    		queue2[count].burT=0; 
+    		time+=queue2[count].burst_time; 
+    		queue2[count].burst_time=0; 
     		flag=1; 
     	} 
-    	else if(queue2[count].burT>0) 
+    	else if(queue2[count].burst_time>0) 
     	{ 
-    		queue2[count].burT-=t2; 
+    		queue2[count].burst_time-=t2; 
     		time+=t2; 
     	} 
-    	if(queue2[count].burT==0&&flag==1) 
+    	if(queue2[count].burst_time==0&&flag==1) 
     	{ 
     		totalProcess2--; 
-    		queue2[count].turnT=time-queue2[count].arrT;
-			queue2[count].waiturnT=queue2[count].turnT-queue2[count].burTcopy; 
-			printf("%d\t|\t%d\t|\t%d\n",queue2[count].pro_name,queue2[count].turnT,queue2[count].waiturnT); 
-    		turnaround_time+=time-queue2[count].arrT; 
-    		wait_time+=time-queue2[count].arrT-queue2[count].burTcopy;
+    		queue2[count].turn_time=time-queue2[count].arrival_time;
+			queue2[count].waiting_time=queue2[count].turn_time-queue2[count].burst_timecopy; 
+			printf("%d\t|\t%d\t|\t%d\n",queue2[count].process_name,queue2[count].turn_time,queue2[count].waiting_time); 
+    		turnaround_time+=time-queue2[count].arrival_time; 
+    		wait_time+=time-queue2[count].arrival_time-queue2[count].burst_timecopy;
     		for(k=count; k<totalProcess2;k++)
     			queue2[k]=queue2[k+1];count--;
     		flag=0; 
@@ -130,5 +130,5 @@ int main()
     		count++; 
     }
     printf("\n Average Waiting Time= %f\n", wait_time/n); 
-    printf("Avg Turnaround Time = %f" ,turnaround_time/n);   
+    printf("Avg Turnaround Time = %f\n" ,turnaround_time/n);   
 }
